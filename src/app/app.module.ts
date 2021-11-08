5b39c42df64cb7d0208f95f36e0b8184f676ebad
Kiril Vedmidskiy <djimeilsobachka@gmail.com>
2021-11-08 11:41:48 +0200

    complete game card and styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule } from '@angular/forms';
@@ -18,7 +19,8 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent
+    SearchBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
